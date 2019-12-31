1e2e884f7eb1f833ae8d494ff4169495ff119f90
houssam ghannoum <houssam_ghannoum@hotmail.com>
2019-12-31 21:41:31 +0200

    commit 12/31/2019

--- Xperience/Xperience/Controllers/PostsController.cs
+++ Xperience/Xperience/Controllers/PostsController.cs
@@ -23,42 +23,43 @@ namespace Xperience.Controllers
         private BaseUser currentUser;
         private ApplicationUser user;
 
-        
+
         public PostsController(ApplicationDbContext dbContext, UserManager<BaseUser> userManager)
         {
             context = dbContext;
             _userManager = userManager;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> OnPostAsync(int pageNumber, int nOfPosts) {
-            currentUser = await _userManager.GetUserAsync(HttpContext.User);
-            user = context.Users.OfType<ApplicationUser>().FirstOrDefault(x => x.Id == currentUser.Id);
+        [HttpPost("{pageNumber},{nOfPosts}")]
+        public IActionResult OnPost(int pageNumber, int nOfPosts)
+        {
+            var Id = _userManager.GetUserId(HttpContext.User);
 
             int skip = nOfPosts * (pageNumber - 1);
 
 
             var posts = context.Posts
             .Where(x => (context.FollowedUsers
-            .FirstOrDefault(n => n.FollowerId == user.Id && n.ApplicationUserId == x.ApplicationUserId)) != null
-            || (context.FollowedSites.FirstOrDefault(n => n.ApplicationUserId == user.Id && n.SiteId == x.SiteId) != null) || x.ApplicationUserId == user.Id)
+            .FirstOrDefault(n => n.FollowerId == Id && n.ApplicationUserId == x.ApplicationUserId)) != null
+            || (context.FollowedSites.FirstOrDefault(n => n.ApplicationUserId == Id && n.SiteId == x.SiteId) != null) || x.ApplicationUserId == Id)
             .OrderByDescending(x => x.postDate).Skip(skip).Take(nOfPosts).ToList();
 
             return Ok(posts);
+
         }
 
         [HttpPost]
-        public async void OnPostAsync(ManagePostModel model) {
-            currentUser = await _userManager.GetUserAsync(HttpContext.User);
-            user = context.Users.OfType<ApplicationUser>().FirstOrDefault(x => x.Id == currentUser.Id);
+        public async void OnPostAsync(ManagePostModel newPost)
+        {
+
 
             var post = new Post()
             {
-                PostDetails = model.PostDetails,
-                ApplicationUserId = user.Id,
-                Caption = model.Caption,
-                SiteId = context.Sites.FirstOrDefault(x => x.Name == model.Site).Id,
-                HashtagId = context.Hashtags.FirstOrDefault(x => x.Name == model.Hashtag).Id,
+                PostDetails = newPost.PostDetails,
+                ApplicationUserId = _userManager.GetUserId(HttpContext.User),
+                Caption = newPost.Caption,
+                SiteId = context.Sites.FirstOrDefault(x => x.Name == newPost.Site).Id,
+                HashtagId = context.Hashtags.FirstOrDefault(x => x.Name == newPost.Hashtag).Id,
                 postDate = DateTime.Now
             };
 
--- Xperience/Xperience/wwwroot/js/feeds.js
+++ Xperience/Xperience/wwwroot/js/feeds.js
@@ -2,43 +2,71 @@
     var posts = {
         init: function () {
             this.cacheDOM();
-            this.loadData(1);
             this.bindFunctions();
+            this.loadData(1);
         },
         cacheDOM: function () {
             this.pageNumber = 1;
             this.$post = $(".posts");
+            this.$postDetails = $("#post_details");
+            this.$caption = $("#caption");
+            this.$site = $("#site");
+            this.$hashtag = $("#hashtag");
+            this.$addPost = $("#add_post");
         },
         bindFunctions: function () {
-            $(window).scroll(function () {
-                
-                if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-                    this.pageNumber += 1;
-                    this.loadData(this.pageNumber)
-                }               
-            }.bind(this));
+            $(window).scroll(this.checkScroll.bind(this));
+            this.$addPost.click(this.createPost.bind(this));
+        },
+        checkScroll: function () {
+            if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+                this.pageNumber++;
+                this.loadData(this.pageNumber);
+            }
         },
         loadData: function (currentPage) {
             $.ajax({
-                url: '/api/Posts?pageNumber=' + currentPage +'&nOfPosts=1',
+                url: '/api/Posts/' + currentPage + ',10',
                 type: 'POST',
                 success: function (data) {
                     this.previewData(data);
                 }.bind(this)
             });
         },
-        previewData(data) {
+        previewData: function (data) {
             $.each(data, function (i, item) {
                 var body = "";
-                body += "<div class='row'>";
-                body += "<div class = 'col-md-4'></div>";
-                body += "<div class='col-md-4 post'>";
+                body += "<div class='row' style='margin-bottom:30px'>";
+                body += "<div class = 'col-md-2'></div>";
+                body += "<div class='col-md-8 post'>";
                 body += "<p>" + item.postDate + "<p>";
                 body += "</div></div>";
                 this.$post.append(body);
             }.bind(this));
+        },
+        createPost: function () {
+            var post = {
+                "PostDetails": this.$postDetails.val(),
+                "Caption": this.$caption.val(),
+                "Site": this.$site.val(),
+                "Hashtag": this.$hashtag.val()
+            }
+            this.insertData(JSON.stringify(post));
+        },
+        insertData: function (data) {
+            console.log(data);
+            $.ajax({
+                url: '/api/Posts?',
+                contentType: 'application/json',
+                data: data,
+                type: 'POST',
+                success: function (returnData) {
+                    this.$post.empty();
+                    this.loadData(1);
+                }.bind(this)
+            });
         }
     }
     posts.init();
-    
+
 })();
